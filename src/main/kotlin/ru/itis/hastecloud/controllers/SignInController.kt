f7aac37e365d257bbc98711a12f339580e1cd766
aiivar <min.aivar2001@gmail.com>
2022-05-23 23:46:51 +0300

    23.05.22 Musa and Aivar co-op #2

--- src/main/kotlin/ru/itis/hastecloud/controllers/SignInController.kt
+++ src/main/kotlin/ru/itis/hastecloud/controllers/SignInController.kt
@@ -1,4 +1,17 @@
 package ru.itis.hastecloud.controllers
 
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+import ru.itis.hastecloud.dtos.LoginDto
+import ru.itis.hastecloud.dtos.forms.LoginForm
+
+
+@RestController
 class SignInController {
+
+    @PostMapping("/login")
+    fun login(form: LoginForm): ResponseEntity<LoginDto>? {
+        return null
+    }
 }
